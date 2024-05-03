faa4d474f104bc8f737f06695bc0ad543f055abc
Jullian0426 <jullian0426@yahoo.com>
2024-05-03 04:12:29 -0500

    Refactor substrings function to handle multiple words

--- Sub_Strings/sub_strings.rb
+++ Sub_Strings/sub_strings.rb
@@ -1,13 +1,16 @@
-def substrings(word, dictionary)
+def substrings(string, dictionary)
   substrings_count = Hash.new(0)
+  stringArray = string.downcase.split
 
   dictionary.each do |substring|
-    if word.downcase.include?(substring.downcase)
-      substrings_count[substring] += 1
+    stringArray.each do |word|
+      if word.include?(substring.downcase)
+        substrings_count[substring] += 1
+      end
     end
   end
   substrings_count
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
